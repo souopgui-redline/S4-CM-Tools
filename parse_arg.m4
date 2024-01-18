c0e6983917e1c33683e5251ba5714f3f48c1c098
Innocent Souopgui <isouopgui@gmail.com>
2024-01-18 15:31:37 -0600

    Added scripts to replace outdated global workflow files

---
+++ parse_arg.m4
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# m4_ignore(
+echo "This is just a script template, not the script (yet) - pass it to 'argbash' to fix this." >&2
+exit 11  #)Created by argbash-init v2.10.0
+# ARG_POSITIONAL_SINGLE([path], [Path to the global flow directory])
+# ARG_DEFAULTS_POS
+# ARG_HELP([<Replaces a couple of files in global workflow>])
+# ARGBASH_GO
+
+# [ <-- needed because of Argbash
+
+# vvv  PLACE YOUR CODE HERE  vvv
+# For example:
+printf "Value of '%s': %s\\n" 'path' "$_arg_path"
+
+# ^^^  TERMINATE YOUR CODE BEFORE THE BOTTOM ARGBASH MARKER  ^^^
+
+# ] <-- needed because of Argbash
---
+++ replace.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+R_ROOT=$(dirname "${0}")
+source ${R_ROOT}/parse_arg.sh
+
+GW_ROOT=${_arg_path}
+printf " -------------------------------------------------\n"
+printf "R_ROOT is ${R_ROOT}\n"
+printf "ls R_ROOT --------------------\n"
+ls ${R_ROOT}
+printf " -------------------------------------------------\n"
+printf "GW_ROOT is ${GW_ROOT}\n"
+printf "ls GW_ROOT --------------------\n"
+ls ${GW_ROOT}
+# ==================== s4 module file for ufs_model.fd
+F_DIR="sorc/ufs_model.fd/modulefiles"
+b_name=ufs_s4.intel.lua
+echo "copying  ${F_DIR}/${b_name}"
+# cp ${R_ROOT}/${F_DIR}/${b_name} ${GW_ROOT}/${F_DIR}/
