942f1a3fdea3933d10bb18abd7f502f9e2f9f10f
Innocent Souopgui <isouopgui@gmail.com>
2024-01-18 15:40:12 -0600

    adjust the script for the location of the replacement

--- replace.sh
+++ replace.sh
@@ -3,6 +3,7 @@
 R_ROOT=$(dirname "${0}")
 source ${R_ROOT}/parse_arg.sh
 
+R_GW_ROOT=${R_ROOT}/gw-replacements
 GW_ROOT=${_arg_path}
 printf " -------------------------------------------------\n"
 printf "R_ROOT is ${R_ROOT}\n"
@@ -16,4 +17,4 @@ ls ${GW_ROOT}
 F_DIR="sorc/ufs_model.fd/modulefiles"
 b_name=ufs_s4.intel.lua
 echo "copying  ${F_DIR}/${b_name}"
-# cp ${R_ROOT}/${F_DIR}/${b_name} ${GW_ROOT}/${F_DIR}/
+# cp ${R_GW_ROOT}/${F_DIR}/${b_name} ${GW_ROOT}/${F_DIR}/
