4b1720150fc10c67a4d4132576f45f70914c84e2
Innocent Souopgui <isouopgui@redlineperf.com>
2024-01-18 21:35:06 +0000

    Added the s4 modulefile for ufs_model.fd

---
+++ gw-replacements/README.txt
@@ -0,0 +1,7 @@
+This directory contains replacement files for global workflow.
+Those replacements are temporary fixes while waiting for the actual fix to propagate.
+
+Replacement file for global workflow,
+from time to time, there is something small that is broken in global workflow,
+fixes take some time to propagate.
+
---
+++ gw-replacements/sorc/ufs_model.fd/modulefiles/ufs_s4.intel.lua
@@ -0,0 +1,23 @@
+help([[
+loads UFS Model prerequisites for S4/Intel
+]])
+
+prepend_path("MODULEPATH", "/data/prod/jedi/spack-stack/spack-stack-1.5.1/envs/unified-env/install/modulefiles/Core")
+
+stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
+load(pathJoin("stack-intel", stack_intel_ver))
+
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.0"
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+
+cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+load(pathJoin("cmake", cmake_ver))
+
+load("ufs_common")
+
+setenv("CC", "mpiicc")
+setenv("CXX", "mpiicpc")
+setenv("FC", "mpiifort")
+setenv("CMAKE_Platform", "s4.intel")
+
+whatis("Description: UFS build environment")
